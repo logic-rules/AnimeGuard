a2d9b16885d4af789e31a43b7a05e8f494ba362e
Shaheer Ali <shaheerzunaira175@gmail.com>
2026-08-11 02:53:43 +0500

    cleaned exisitng code and added comments where suited

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import os
-from extract_frames import extract_frames
-from clear_frames import clear_frames
+from extract_frames import extract_frames # frame extraction
+from clear_frames import clear_frames # frame clearance
 
 
 clear_frames()
 
 files = os.listdir("input")
-if not files:
+if not files:                  # i.e. if line 8... previous line is just NOT!
     print("Error: No files found in input folder.")
     exit()
 
-first_file = files[0]
+first_file = files[0]     # (we want just ONE SINGLE FILE per once to proceed)
 
 video_extensions = (".mp4", ".mkv", ".avi", ".mov", ".webm")
 
-if not first_file.lower().endswith(video_extensions):
+if not first_file.lower().endswith(video_extensions): # checks extensions (and lowercase handling at that too)
     print("Error: Input file is not a supported video.")
     exit()
 
