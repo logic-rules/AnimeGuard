6745558e1c32e18c57ea00bada56e370d03ca8e1
Shaheer Ali <shaheerzunaira175@gmail.com>
2026-08-11 02:11:12 +0500

    Add input folder validation

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import os
 from extract_frames import extract_frames
 from clear_frames import clear_frames
 
+
 clear_frames()
 
 files = os.listdir("input")
+if not files:
+    print("Error: No files found in input folder.")
+    exit()
 first_file = files[0]
 video_path = os.path.join("input", first_file)
 
