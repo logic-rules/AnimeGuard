40b060e20a48d6782d874bd86684b793b9916a9e
Shaheer Ali <shaheerzunaira175@gmail.com>
2026-08-19 23:20:53 +0500

    adjusted values & structured

--- extract_frames.py
+++ extract_frames.py
@@ -3,8 +3,8 @@ def extract_frames(video_path):
 
     subprocess.run([
     "ffmpeg", "-i", video_path,
-    "-vf", "select='gt(scene,0.2)'",
+    "-vf", "select='gt(scene,0.15)', scale=-2:480",
     "-vsync", "vfr",
               "-y",
-    "frames/frame_%03d.jpg"
+    "frames/frame_%04d.jpg"
 ])
